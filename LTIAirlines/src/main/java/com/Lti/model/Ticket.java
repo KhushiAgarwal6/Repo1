0ad04244b7ff27668e08d32d32424ab170c9c58d
divya280898 <divyasingh280898@gmail.com>
2021-09-15 12:53:21 +0530

    Ticket class

---
+++ LTIAirlines/src/main/java/com/Lti/model/Ticket.java
@@ -0,0 +1,20 @@
+package com.Lti.model;
+
+public class Ticket {
+   int TId;
+   String name;
+public int getTId() {
+	return TId;
+}
+public void setTId(int tId) {
+	TId = tId;
+}
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+   
+   
+}
